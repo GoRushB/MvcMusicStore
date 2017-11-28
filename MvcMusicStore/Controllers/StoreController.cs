eaf4e5ba8f2a815017317d7a147b0400d93fc423
HanShengLuo <smgrushb@gmail.com>
2017-11-28 14:27:08 +0800

    添加项目文件。

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,42 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusinStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var genres = storeDB.Genres.ToList();
+
+            return this.View(genres);
+        }
+        // GET: /Store/GenreMenu
+        [ChildActionOnly]
+        public ActionResult GenreMenu()
+        {
+            var genres = storeDB.Genres.ToList();
+            return PartialView(genres);
+        }
+        // /Store/Browse?genre=DISCO
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
+
+            return this.View(genreModel);
+        }
+        // /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var album = storeDB.Albums.Find(id);
+            return View(album);
+        }
+
+    }
+}
---
+++ MvcMusicStore/Controllers/StoreManagerController.cs
@@ -0,0 +1,85 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    //[Authorize(Roles = "Administrator")]
+    public class StoreManagerController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        // GET: StoreManager
+        public ActionResult Index()
+        {
+            var albums = storeDB.Albums.Include("Genre").Include("Artist");
+            return View(albums.ToList());
+        }
+        // GET: StoreManager/Details/5
+        public ActionResult Details(int id)
+        {
+            MvcMusicStore.Models.Album album = storeDB.Albums.Find(id);
+            return View(album);
+        }
+        // GET: StoreManager/Create
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(storeDB.Genres, "GenreId", "Name");
+            ViewBag.ArtistId = new SelectList(storeDB.Artists, "ArtistId", "Name");
+            return View();
+        }
+        // POST: StoreManager/Create
+        [HttpPost]
+        public ActionResult Create(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                storeDB.Albums.Add(album);
+                storeDB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(storeDB.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(storeDB.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+        // POST: /StoreManager/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Album album = storeDB.Albums.Find(id);
+            ViewBag.GenreId = new SelectList(storeDB.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(storeDB.Artists, "ArtistId", "Name", album.ArtistId);
+            return this.View(album);
+        }
+        // POST: /StoreManager/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                storeDB.Entry(album).State = System.Data.Entity.EntityState.Modified;
+                storeDB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(storeDB.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(storeDB.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+        // POST: /StoreManager/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Album album = storeDB.Albums.Find(id);
+            return View(album);
+        }
+        // POST: /StoreManager/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Album album = storeDB.Albums.Find(id);
+            storeDB.Albums.Remove(album);
+            storeDB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
